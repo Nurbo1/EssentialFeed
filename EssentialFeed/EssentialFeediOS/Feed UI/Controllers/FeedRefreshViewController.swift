b9b46909d48ba256df2dcfe102d91ec4c9cc97ce
Nurbol <nurbol23072002@gmail.com>
2026-02-02 12:13:49 +0500

    Extract "FeedImageCell" creation/configuration and image loading logic with "FeedImageDataLoader" to the new "FeedImageCellController"

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,27 @@
+
+
+public final class FeedRefreshViewController: NSObject {
+    public lazy var view: UIRefreshControl = {
+       let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    private var feedLoader: FeedLoader
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
